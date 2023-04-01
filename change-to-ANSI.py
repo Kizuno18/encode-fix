1db8940ed4dbd0d438279180ec5556b26043e5ed
Kizuno18 <110933270+Kizuno18@users.noreply.github.com>
2023-04-01 08:03:04 -0300

    Create change-to-ANSI.py

---
+++ change-to-ANSI.py
@@ -0,0 +1,23 @@
+import os
+import codecs
+import chardet
+
+def converte_arquivos(path):
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            converte_arquivos(item_path)
+        else:
+            if item.endswith('.lua') or item.endswith('.xml'):
+                with open(item_path, 'rb') as f:
+                    conteudo = f.read()
+                    encoding = chardet.detect(conteudo)['encoding']
+                    if encoding is None:
+                        encoding = 'utf-8'
+                conteudo_str = conteudo.decode(encoding)
+                conteudo_codificado = conteudo_str.encode('ANSI')
+                with open(item_path, 'wb') as f:
+                    f.write(conteudo_codificado)
+
+# exemplo de uso
+converte_arquivos('/caminho/da/pasta')
