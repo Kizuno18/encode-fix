4f94972b9a4c5187b200b6dbfe2de04d335aeab2
Kizuno18 <110933270+Kizuno18@users.noreply.github.com>
2023-04-01 07:58:13 -0300

    Create changer.py

---
+++ changer.py
@@ -0,0 +1,21 @@
+import os
+import codecs
+import chardet
+
+def converte_arquivos(path):
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            converte_arquivos(item_path)
+        else:
+            if item.endswith('.lua') or item.endswith('.xml'):
+                with open(item_path, 'rb') as f:
+                    conteudo = f.read()
+                    encoding = chardet.detect(conteudo)['encoding']
+                    if encoding is None:
+                        encoding = 'utf-8'
+                with codecs.open(item_path, 'w', encoding=encoding) as f:
+                    f.write(conteudo.decode(encoding))
+                    
+# exemplo de uso
+converte_arquivos('/caminho/da/pasta')
