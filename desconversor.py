e6f039b35b89622eb4d82f57a2a5b29b6cff6453
Kizuno18 <110933270+Kizuno18@users.noreply.github.com>
2023-04-02 13:52:19 -0300

    Create desconversor.py

---
+++ desconversor.py
@@ -0,0 +1,23 @@
+import os
+import codecs
+import chardet
+
+def converte_arquivos(path):
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            converte_arquivos(item_path)
+        else:
+            if item.endswith('.lua'):
+                with open(item_path, 'rb') as f:
+                    conteudo = f.read()
+                    encoding = chardet.detect(conteudo)['encoding']
+                    if encoding is None:
+                        encoding = 'utf-8'
+                conteudo_str = conteudo.decode(encoding, errors='ignore')
+                conteudo_codificado = conteudo_str.encode('ANSI', errors='ignore')
+                with open(item_path, 'wb') as f:
+                    f.write(conteudo_codificado)
+
+# exemplo de uso
+converte_arquivos('C:\\Users\\oBigLeo\\Desktop\\serv\\')
